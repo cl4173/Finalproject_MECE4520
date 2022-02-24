4e6bb35aabf2b4127a964e80bbd849756376ed61
cl4173 <92229931+cl4173@users.noreply.github.com>
2022-02-23 22:02:25 -0500

    Add files via upload

---
+++ Final_Model_Ridge.py
@@ -0,0 +1,112 @@
+# Import libs
+from matplotlib.colors import rgb_to_hsv
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy.core.fromnumeric import mean
+import pandas as pd
+from pandas.tseries.offsets import BYearBegin
+import seaborn as sns
+import time
+from pandas.core.dtypes.missing import isnull
+from pandas.core.indexes.base import Index, ensure_index
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn import metrics
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d
+import warnings
+#warnings.filterwarnings("ignore")
+
+# Import data_B & clean
+URL=r'C:\Users\liuch\Desktop\Courses\Data_Science_MECE4520\Project\diabetes_binary_health_indicators_BRFSS2015.csv'
+I_Data_B=pd.read_csv(URL)
+I_Data_B=pd.DataFrame(I_Data_B,columns=I_Data_B.columns)
+I_Data_B.fillna(0)
+for b in range(2,5):   # Add high orders components (5)         
+    I_Data_B[f"BMI{b}"] =I_Data_B["BMI"].apply(lambda x: x**(b))
+    I_Data_B[f"GenHlth{b}"] =I_Data_B["GenHlth"].apply(lambda x: x**(b))
+    I_Data_B[f"MentHlth{b}"] =I_Data_B["MentHlth"].apply(lambda x: x**(b))
+    I_Data_B[f"PhysHlth{b}"] = I_Data_B["PhysHlth"].apply(lambda x: x**(b))
+    I_Data_B[f"Age{b}" ] = I_Data_B["Age"].apply(lambda x: x**(b))
+    I_Data_B[f"Education{b}" ] = I_Data_B["Education"].apply(lambda x: x**(b))
+    I_Data_B[f"Income{b}"] = I_Data_B["Income"].apply(lambda x: x**(b))
+Data_B=pd.DataFrame(I_Data_B,columns=I_Data_B.columns)
+#Define Variables
+
+N_trails=1   #Define loops(random seed)
+P_C=0.05         # Set penalty C start point
+R_solver='liblinear' # Define regression solver，newton-cg’, ‘lbfgs’, ‘liblinear’, ‘sag’, ‘saga’
+R_type_A=3      # Define regression type, 0-normal, 1-ridge, 2-lasso, 3-elastic net
+Tol=1e-5      # Set tolerance
+M_iter=22000   # Set max iter number
+
+H_Parameter_Kfold=pd.DataFrame(columns=["K","train","C", "validation",'time']) #Define output of Lasso
+
+#package function
+def Regression(n_trail:int,p_c:float,Itol:float,r_type:int,r_solver:str,m_iter:int,data):
+    I_data=pd.DataFrame(data,columns=data.columns)
+    a=0            #Define trail counting number 
+    I_solver=r_solver
+    I_p_c=p_c
+    I_penalty='l1'
+    H_parameter_kfold=pd.DataFrame(columns=["K", "train", "validation",'time'])
+    
+    for i in range(n_trail):
+        a=a+1            #Define K loop counting number
+        #print(f"L1 Ratio:{E_ratio} Loop:{a}") # Count loop number
+        start_trail=time.time()
+        #Train and validate
+        I_data_x=I_data.drop(["Diabetes_binary"],axis=1) # Get X
+        I_data_y=I_data["Diabetes_binary"]
+        x_tr, x_te, y_train, y_test = train_test_split(I_data_x, I_data_y, test_size=0.0005, random_state=(19)) 
+        x_BS=StandardScaler()#Normalize data
+        X_tr=x_BS.fit_transform(x_tr)
+        X_te=x_BS.transform(x_te) 
+        #print(f"L1 Ratio:{I_l1_ratio} in K_fold Loop:{a}")
+        #print(I_data.shape)
+        # Fit model
+        H_all_models = []
+        #Start fit process
+        start_Kfold=time.time()
+        H_model=sklearn.linear_model.LogisticRegression(penalty=I_penalty,dual=False,solver=I_solver,intercept_scaling=1,
+        tol=Itol,C=I_p_c,max_iter=m_iter)                          # Logistic regression
+        H_model.fit(X_tr,y_train)# Train model
+        Y_predict_T=H_model.predict_proba(X_tr) # Predict value of train dataset
+        Y_predict_V=H_model.predict_proba(X_te)# Predict value of validation dataset
+        Y_los_T=round(sklearn.metrics.log_loss(y_train,Y_predict_T),4)# calculate los for train sets
+        Y_los_V=round(sklearn.metrics.log_loss(y_test,Y_predict_V),4)# calculate los for validation sets
+  
+        fpr, tpr, thresholds = metrics.roc_curve(y_test,Y_predict_V[:,1],pos_label=1)# Calculate ROC
+        auc=metrics.auc(fpr, tpr)
+        end_Kfold=time.time()
+        Input_row={"K": a, "train":Y_los_T, "validation":Y_los_V,'time':round((end_Kfold-start_Kfold),4)}#Collect input row
+        H_parameter_kfold=H_parameter_kfold.append(Input_row,ignore_index=True)#Collect input information into table
+        H_all_models.append(H_model)   
+        # Get trail results
+        end_trail=time.time()
+        
+    return H_parameter_kfold,auc,fpr,tpr,thresholds
+
+#main calc
+H_Parameter_Kfold,AUC,FPR,TPR,Thresholds=Regression(n_trail=N_trails,p_c=P_C,Itol=Tol,r_type=R_type_A,
+r_solver=R_solver,m_iter=M_iter,data=Data_B)      
+#Print results
+print("------------------Results------------------")
+print(H_Parameter_Kfold)
+print(f'AUC is {round(AUC,4)}')
+print("-------------------------------------------")
+
+#Print results
+AA=plt.figure()
+sns.lineplot(x=FPR, y=TPR,  label="ROC")
+sns.lineplot(x=[0, 1], y=[0, 1], linestyle="--")
+plt.gca().set_ylabel("TPR")
+plt.gca().set_xlabel("FPR")
+plt.title('ROC of Ridge Regression')
+plt.tight_layout()
+
+plt.show()
---
+++ Random Forest Project_final_model.py
@@ -0,0 +1,96 @@
+import numpy as np
+from numpy.lib.npyio import zipfile_factory
+import pandas as pd
+from matplotlib.colors import rgb_to_hsv
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import mean
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import joblib
+from sklearn import metrics
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d
+import seaborn as sns
+import sklearn
+import time
+data = pd.read_csv(r'C:\Users\liuch\Desktop\Courses\Data_Science_MECE4520\Project\diabetes_binary_health_indicators_BRFSS2015.csv')
+data=pd.DataFrame(data)
+# divide data into attributes and labels
+X = data.drop(["Diabetes_binary"],axis=1)
+y = data["Diabetes_binary"]
+for j in range(0,2):
+    X[f"BMI{j+2}"] =X["BMI"].apply(lambda x: x**(j+2))
+    X[f"GenHlth{j+2}"] =X["GenHlth"].apply(lambda x: x**(j+2))
+    X[f"MentHlth{j+2}"] = X["MentHlth"].apply(lambda x: x**(j+2))
+    X[f"PhysHlth{j+2}"] = X["PhysHlth"].apply(lambda x: x**(j+2))
+    X[f"Age{j+2}"] = X["Age"].apply(lambda x: x**(j+2))
+    X[f"Education{j+2}"] = X["Education"].apply(lambda x: x**(j+2))
+    X[f"Income{j+2}"] = X["Income"].apply(lambda x: x**(j+2))
+# Divide into training and testing data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.0005, random_state = 42)
+
+# Scale the data
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+print(X_train.shape)
+print(X_test.shape)
+#Define default characters,please keep
+Oob_score=True
+Bootstrap=True
+N_jobs=-1
+Ccp_alpha=0
+Max_leaf_nodes=None
+''' Define variables, please pick no more than two variables 
+------------------------------------------------------------'''
+
+Criterion='gini'       # {“gini”, “entropy”}
+Max_features='sqrt'    #{“auto”, “sqrt”, “log2”}
+N_estimators=600      # set Tree number(start point),int
+Max_depth=100          # set Max depth start point,int
+Min_samples_leaf=76     # set Min depth start point,int
+Min_samples_split=100    # set Min sample split start point,int
+'''-------------------end of modify--------------------------'''
+
+#Define output
+Parameter=pd.DataFrame(columns=['Oob_score',"AUC","Los","Time"])
+Roc=pd.DataFrame(columns=["fpr","tpr"])
+Start = time.time()
+regressor = RandomForestClassifier(criterion="gini", n_estimators = N_estimators,max_features=Max_features,max_depth=Max_depth,
+max_leaf_nodes=Max_leaf_nodes,min_samples_leaf=Min_samples_leaf,min_samples_split=Min_samples_split,
+bootstrap=Bootstrap,n_jobs=N_jobs,ccp_alpha=Ccp_alpha,random_state = 19)
+regressor.fit(X_train, y_train)
+Oob_Score=regressor.oob_score
+y_pred = regressor.predict_proba(X_test)
+End=time.time()
+#calculate result
+Los=round(sklearn.metrics.log_loss(y_test,y_pred),4)
+fpr, tpr, thresholds = metrics.roc_curve(y_test,y_pred[:,1],pos_label=1)
+AUC=metrics.auc(fpr, tpr)
+#output
+Input_row_P={'Oob_score':Oob_Score,"AUC":round(AUC,4),"Los":Los,"Time":round((End-Start),4)}
+Input_row_R={"fpr":fpr,"tpr":tpr}
+Parameter=Parameter.append(Input_row_P,ignore_index=True)
+Roc=Roc.append(Input_row_R,ignore_index=True)
+    
+print(y_pred)
+#print results      
+print(Parameter)
+#draw pictures
+AA=plt.figure()
+sns.lineplot(x=fpr, y=tpr,  label="ROC")
+sns.lineplot(x=[0, 1], y=[0, 1], linestyle="--")
+plt.gca().set_ylabel("TPR")
+plt.gca().set_xlabel("FPR")
+plt.title('ROC of RandomForest')
+plt.tight_layout()
+plt.tight_layout()
+plt.show()
+
+
+#save data
+#S_URL=(r'C:\Users\liuch\Desktop\R\{}.csv'.format(str(V1_name)))
+#Parameter.to_csv(S_URL)
